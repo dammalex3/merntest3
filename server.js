12f89d0ee15a6acbfae0061e912157c40b7077bc
Alex <adamm@urbn.com>
2019-04-30 23:51:53 -0400

    server.js change

--- server.js
+++ server.js
@@ -110,6 +110,17 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
